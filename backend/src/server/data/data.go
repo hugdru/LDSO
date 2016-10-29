0a63e80f7775049e168dcee4e91af0e20dd8f1c5
francisquinha <francisquinha@gmail.com>
2016-10-29 12:28:55 +0100

    database for criteria

--- backend/src/server/data/data.go
+++ backend/src/server/data/data.go
@@ -1,31 +1,31 @@
 package data
 
 type Group struct {
-	Name string `json:name`
-	Weight uint `json:weight`
+	Name string `json:"name"`
+	Weight uint `json:"weight"`
+	Sub_Groups []Sub_Group `json:"sub_groups"`
 }
 
 type Sub_Group struct {
-	Name string `json:name`
-	Weight uint `json:weight`
-	Criteria []Criterion `json:criteria`
+	Name string `json:"name"`
+	Weight uint `json:"weight"`
+	Criteria []Criterion `json:"criteria"`
 }
-
-type Accessibility string
 
 type Criterion struct {
-	Name string `json:name`
-	Accessibility Accessibility `json:accessibility`
-	Legislation bool `json:legislation`
+	Name string `json:"name"`
+	Weight uint `json:"weight"`
+	Accessibilities []Accessibility `json:"accessibilities"`
+	Legislation string `json:"legislation"`
 }
 
-type Group_Set struct {
-	Group Group `json:group`
-	Sub_Groups []Sub_Group `json:sub_groups`
+type Accessibility struct {
+	Name string `json:"name"`
+	Weight uint `json:"weight"`
 }
 
 type Owner struct {
-	Name string `json:name`
+	Name string `json:"name"`
 }
 
 type Property struct {
@@ -35,15 +35,15 @@ type Property struct {
 }
 
 type Note struct {
-	Data string `json:data`
-	Criterion Criterion `json:sub_criterion`
-	Image_path string `json:image_path`
+	Data string `json:"data"`
+	Criterion Criterion `json:"sub_criterion"`
+	Image_path string `json:"image_path"`
 }
 
 type Evaluation struct {
-	Property Property `json:property`
-	Notes []Note `json:notes`
-	Value uint `json:value`
+	Property Property `json:"property"`
+	Notes []Note `json:"notes"`
+	Value uint `json:"value"`
 }
 
 func (this *Sub_Group) SetCriteria(subs ...Criterion) {
@@ -57,13 +57,13 @@ func (this *Sub_Group) AppendCriteria(sub Criterion) {
 	this.Criteria = append(this.Criteria, sub)
 }
 
-func (this *Group_Set) SetSubs(subs ...Sub_Group) {
+func (this *Group) SetSubs(subs ...Sub_Group) {
 	this.Sub_Groups = nil
 	for _, sub := range subs {
 		this.Sub_Groups = append(this.Sub_Groups, sub)
 	}
 }
 
-func (this *Group_Set) AppendSub(sub Sub_Group) {
+func (this *Group) AppendSub(sub Sub_Group) {
 	this.Sub_Groups = append(this.Sub_Groups, sub)
 }
