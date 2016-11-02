69dccb8f5571ea2a5432a1c191caddb679d5e2e9
jfpmonteiro <jose.filipe.m@gmail.com>
2016-11-02 17:22:52 +0000

    Updated classes to reflect changes made to server.

--- frontend/src/app/group/group.ts
+++ frontend/src/app/group/group.ts
@@ -1,6 +1,7 @@
 import { SubGroup } from 'sub-group/sub-group';
 
 export class Group {
+	id: number;
 	name: string;
 	weight: number;
 	sub_groups: SubGroup[];
--- frontend/src/app/sub-group/sub-group.ts
+++ frontend/src/app/sub-group/sub-group.ts
@@ -1,6 +1,7 @@
 import { Criterion } from 'criterion/criterion';
 
 export class SubGroup {
+	id: number;
 	name: string;
 	weight: number;
 	criteria: Criterion[];
