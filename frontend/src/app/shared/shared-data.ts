9a0a176fc8df1962684ae3898df126412d946afe
jfpmonteiro <jose.filipe.m@gmail.com>
2016-11-03 22:41:54 +0000

    Shared data now contains the proper urls.

--- frontend/src/app/shared/shared-data.ts
+++ frontend/src/app/shared/shared-data.ts
@@ -1,3 +1,21 @@
 export const serverUrl = 'http://localhost:8080';
-export const getGroupUrl = this.serverUrl + '/getGroups';
-export const setGroupUrl = this.serverUrl + '/setGroup';
+
+export const getGroupsUrl = this.serverUrl + '/getAllMainGroups';
+export const getGroupUrl = this.serverUrl + '/getOneMainGroup';
+export const updateGroupUrl = this.serverUrl + '/updateMainGroup';
+export const setGroupUrl = this.serverUrl + '/setMainGroup';
+
+export const getSubGroupsUrl = this.serverUrl + '/getAllSubGroups';
+export const getSubGroupUrl = this.serverUrl + '/getOneSubGroup';
+export const updateSubGroupUrl = this.serverUrl + '/updateSubGroup';
+export const setSubGroupUrl = this.serverUrl + '/setSubGroup';
+
+export const getCriteriaUrl = this.serverUrl + '/getAllCriteria';
+export const getCriterionUrl = this.serverUrl + '/getOneCriterion';
+export const updateCriterionUrl = this.serverUrl + '/updateCriterion';
+export const setCriterionUrl = this.serverUrl + '/setCriterion';
+
+export const getAccessibilitiesUrl = this.serverUrl + '/getAllAccessibilities';
+export const getAccessibilityUrl = this.serverUrl + '/getOneAccessibility';
+export const updateAccessibilityUrl = this.serverUrl + '/updateAccessibility';
+export const setAccessibilityUrl = this.serverUrl + '/setAccessibility';
