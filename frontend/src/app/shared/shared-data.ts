87646825a32f12d7edb4a8110db047375265bc69
jfpmonteiro <jose.filipe.m@gmail.com>
2016-11-04 11:40:38 +0000

    Finished group and sub group services.

--- frontend/src/app/shared/shared-data.ts
+++ frontend/src/app/shared/shared-data.ts
@@ -10,13 +10,16 @@ export const getSubGroupsUrl = this.serverUrl + '/getAllSubGroups';
 export const getSubGroupUrl = this.serverUrl + '/getOneSubGroup';
 export const updateSubGroupUrl = this.serverUrl + '/updateSubGroup';
 export const setSubGroupUrl = this.serverUrl + '/setSubGroup';
+export const removeSubGroupUrl = this.serverUrl + '/removeSubMainGroup';
 
 export const getCriteriaUrl = this.serverUrl + '/getAllCriteria';
 export const getCriterionUrl = this.serverUrl + '/getOneCriterion';
 export const updateCriterionUrl = this.serverUrl + '/updateCriterion';
 export const setCriterionUrl = this.serverUrl + '/setCriterion';
+export const removeCriterionUrl = this.serverUrl + '/removeCriterion';
 
 export const getAccessibilitiesUrl = this.serverUrl + '/getAllAccessibilities';
 export const getAccessibilityUrl = this.serverUrl + '/getOneAccessibility';
 export const updateAccessibilityUrl = this.serverUrl + '/updateAccessibility';
 export const setAccessibilityUrl = this.serverUrl + '/setAccessibility';
+export const removeAccessibilityUrl = this.serverUrl + '/removeAccessibility';
--- frontend/src/app/sub-group/service/sub-group.service.ts
+++ frontend/src/app/sub-group/service/sub-group.service.ts
@@ -5,25 +5,35 @@ import { Observable } from 'rxjs/Observable';
 import {
 	getSubGroupsUrl,
 	getSubGroupUrl,
-	setSubGroupUrl
+	updateSubGroupUrl,
+	setSubGroupUrl,
+	removeSubGroupUrl
 } from 'shared/shared-data';
 import { HandlerService } from 'handler.service';
 import { SubGroup } from 'sub-group/sub-group';
 
 @Injectable()
 export class SubGroupService {
-	 constructor(private handler: HandlerService) { }
+	constructor(private handler: HandlerService) { }
 
-	 getSubGroups(): Observable<SubGroup[]> {
-		 return this.handler.get<SubGroup[]>(getSubGroupsUrl);
-	 }
+	getSubGroups(): Observable<SubGroup[]> {
+	    return this.handler.get<SubGroup[]>(getSubGroupsUrl);
+	}
 
-	 getSubGroup(tag: string, type: string, value: any): Observable<SubGroup> {
-		 return this.handler.get<SubGroup>(getSubGroupUrl + "?tag=" + tag
-			   + "&type=" + type + "&value=" + value);
-	 }
+	getSubGroup(tag: string, type: string, value: any): Observable<SubGroup> {
+	    return this.handler.getOne<SubGroup>(getSubGroupUrl, tag, type, value);
+	}
 
-	 setSubGroup(subGroup: SubGroup): Observable<Response> {
-		 return this.handler.set<SubGroup>(subGroup, setSubGroupUrl);
-	 }
+	updateSubGroup(id: number, tag: string, type: string, value: any)
+			: Observable<Response> {
+		return this.handler.update(updateSubGroupUrl, id, tag, type, value);
+	}
+
+	setSubGroup(subGroup: SubGroup): Observable<Response> {
+	    return this.handler.set<SubGroup>(subGroup, setSubGroupUrl);
+	}
+
+	removeSubGroup(id: number): Observable<Response> {
+		return this.handler.delete(removeSubGroupUrl, id);
+	}
 }
