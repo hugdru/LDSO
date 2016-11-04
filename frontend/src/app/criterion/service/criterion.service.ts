25bdc89f884a0fe8dda73ad5c7a0922d5ee9399b
Filipa Brito <Filipa Brito>
2016-11-04 13:34:57 +0000

    Services and Tests concluded.
    Added criterion, accessibility and subgroup Services the update and
    remove functions.
    Tests implemented for the refered.
    Renamed tests.

--- frontend/src/app/criterion/service/criterion.service.ts
+++ frontend/src/app/criterion/service/criterion.service.ts
@@ -5,7 +5,9 @@ import { Observable } from 'rxjs/Observable';
 import {
 	getCriteriaUrl,
 	getCriterionUrl,
-	setCriterionUrl
+	updateCriterionUrl,
+	setCriterionUrl,
+	removeCriterionUrl
 } from 'shared/shared-data';
 import { HandlerService } from 'handler.service';
 import { Criterion } from 'criterion/criterion';
@@ -20,12 +22,21 @@ export class CriterionService {
 
 	getCriterion(tag: string, type: string, value: any)
 			: Observable<Criterion> {
-		return this.handler.get<Criterion>(getCriterionUrl + "?tag=" + tag
-				+ "&type=" + type + "&value=" + value);
+		return this.handler.getOne<Criterion>(getCriterionUrl, tag, type,
+				value);
+	}
+
+	updateCriterion(id: number, tag: string, type: string, value: any)
+			:Observable<Response> {
+		return this.handler.update(updateCriterionUrl, id, tag, type, value);
 	}
 
 	setCriterion(criterion: Criterion): Observable<Response> {
 		return this.handler.set<Criterion>(criterion, setCriterionUrl);
+	}
+
+	removeCriterion(id: number): Observable<Response> {
+		return this.handler.delete(removeCriterionUrl, id);
 	}
 
 }
