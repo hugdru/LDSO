e8b4ac20ce9e559b459f59ea0a547973f06e07df
Hugo Drumond <hugo.drumond@fe.up.pt>
2016-11-19 00:32:46 +0000

    database schema, class diagram tweaks, dkr variadic, docker for postgres

--- backend/src/server/server.go
+++ backend/src/server/server.go
@@ -4,17 +4,10 @@ import (
 	"github.com/pressly/chi"
 	"github.com/rs/cors"
 	"net/http"
-	"server/conn"
-	"server/db"
 )
 
 func main() {
-	dbSession := db.Connect("mongodb:27017")
-	defer db.Disconnect(dbSession)
 	router := chi.NewRouter()
-
-	db.SetCollections(dbSession, "Places4All", "main_group", "sub_group",
-		"criterion", "accessibility", "property", "audit")
 
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
@@ -25,22 +18,7 @@ func main() {
 	})
 	router.Use(cors.Handler)
 
-	setRouter(router, "/mainGroups", "main_group")
-	setRouter(router, "/subGroups", "sub_group")
-	setRouter(router, "/criteria", "criterion")
-	setRouter(router, "/accessibilities", "accessibility")
-	setRouter(router, "/audits", "audit")
-	setRouter(router, "/properties", "property")
-
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		panic(err)
 	}
-}
-
-func setRouter(router *chi.Mux, url, coll string) {
-	router.Get(url, conn.Get(db.GetCollection(coll)))
-	router.Get(url+"/find", conn.GetOne(db.GetCollection(coll)))
-	router.Post(url, conn.Set(db.GetCollection(coll)))
-	router.Put(url, conn.Update(db.GetCollection(coll)))
-	router.Delete(url, conn.Delete(db.GetCollection(coll)))
 }
