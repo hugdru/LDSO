eb626a0f6e2e3dc2cbf867c64715266f651b53eb
francisquinha <francisquinha@gmail.com>
2016-11-20 20:58:43 +0000

    audit component select subgroups changed selected subgroup ids to selected subgroups in order to fit with the rest of the audit component missing the correct action to send the subgroups to a new audit.

--- frontend/src/app/audit/audit-select.component.ts
+++ frontend/src/app/audit/audit-select.component.ts
@@ -1,66 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
-import { MainGroup } from 'main-group/main-group';
-import { SubGroup } from "../sub-group/sub-group";
-import { MainGroupService } from 'main-group/service/main-group.service';
-import { SubGroupService } from "../sub-group/service/sub-group.service";
+import {MainGroup} from 'main-group/main-group';
+import {SubGroup} from "../sub-group/sub-group";
+import {MainGroupService} from 'main-group/service/main-group.service';
+import {SubGroupService} from "../sub-group/service/sub-group.service";
 
 @Component({
-	selector: 'p4a-audit',
-	templateUrl: './html/audit-select.component.html',
-	styleUrls: ['./audit.component.css'],
-	providers: [MainGroupService, SubGroupService]
+    selector: 'p4a-audit',
+    templateUrl: './html/audit-select.component.html',
+    styleUrls: ['./audit.component.css'],
+    providers: [MainGroupService, SubGroupService]
 })
 
 export class AuditSelectComponent implements OnInit {
 
-	property_id: number;
-	mainGroups: MainGroup[];
-	subGroups: SubGroup[];
-	errorMsg: string;
-	selectedSubgroups: number[];
+    property_id: number;
+    mainGroups: MainGroup[];
+    subGroups: SubGroup[];
+    errorMsg: string;
+    selectedSubGroups: SubGroup[];
+    @Output() onAction = new EventEmitter<SubGroup[]>();
 
-	constructor(
-		private mainGroupService: MainGroupService,
-		private subGroupService: SubGroupService,
-		private route: ActivatedRoute
-	) { }
+    constructor(private mainGroupService: MainGroupService,
+                private subGroupService: SubGroupService,
+                private route: ActivatedRoute) {
+    }
 
-	ngOnInit(): void {
-		this.property_id = +this.route.snapshot.params['id'];
-		this.initMainGroups();
-        this.selectedSubgroups = [];
-	}
+    ngOnInit(): void {
+        this.property_id = +this.route.snapshot.params['id'];
+        this.initMainGroups();
+        this.subGroups = [];
+        this.selectedSubGroups = [];
+    }
 
-	initMainGroups(): void {
-		this.mainGroupService.getMainGroups().subscribe(
-			data => this.mainGroups = data,
-			error => this.errorMsg = <any> error
-		);
-	}
+    initMainGroups(): void {
+        this.mainGroupService.getMainGroups().subscribe(
+            data => this.mainGroups = data,
+            error => this.errorMsg = <any> error
+        );
+    }
 
-	showChildren(mainGroup: MainGroup): void {
-		this.subGroupService.getSomeSubGroups("main_group", "int", mainGroup._id).subscribe(
-			data => this.subGroups = data
-		);
-	}
+    showChildren(mainGroup: MainGroup): void {
+        this.subGroupService.getSomeSubGroups("main_group", "int", mainGroup._id).subscribe(
+            data => this.subGroups = data
+        );
+    }
 
-    toggleSubGroup(id: number): void {
-        var index = this.selectedSubgroups.indexOf(id);
+    toggleSubGroup(subGroup: SubGroup): void {
+        var index = this.selectedSubGroups.map(function (x) {return x._id;}).indexOf(subGroup._id);
         if (index > -1) {
-            this.selectedSubgroups.splice(index, 1);
+            this.selectedSubGroups.splice(index, 1);
         }
         else {
-            this.selectedSubgroups.push(id);
+            this.selectedSubGroups.push(subGroup);
         }
     }
 
-   checkedSubGroup(id: number): boolean {
-       if (this.selectedSubgroups.indexOf(id) > -1) {
-           return true;
-       }
-       return false;
-   }
+    checkedSubGroup(subGroup: SubGroup): boolean {
+        var index = this.selectedSubGroups.map(function (x) {return x._id;}).indexOf(subGroup._id);
+        if (index > -1) {
+            return true;
+        }
+        return false;
+    }
+
+    pressed(): void {
+        this.onAction.emit(this.selectedSubGroups);
+    }
 
 }
