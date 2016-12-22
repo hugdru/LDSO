1b24a1bf4cf936c08d6724373cba1494cd5a25e7
Hugo Drumond <hugo.drumond@fe.up.pt>
2016-12-22 23:01:06 +0000

    image upload refactor, sessionData refactor

---
+++ backend/src/server/handler/sessionData/sessionData.go
@@ -0,0 +1,45 @@
+package sessionData
+
+import (
+	"server/datastore"
+	"net/http"
+	"github.com/alexedwards/scs/session"
+	"encoding/gob"
+)
+
+const EntityKey = "entity"
+
+func Init() {
+	gob.Register(SessionData{})
+}
+
+type SessionData struct {
+	Id       int64
+	Username string
+	Email    string
+	Name     string
+	Country  string
+}
+
+func SetSessionData(entity *datastore.Entity, w http.ResponseWriter, r *http.Request) *SessionData {
+	// Preventing session fixation
+	err := session.RegenerateToken(r)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return nil
+	}
+
+	sessionData := &SessionData{
+		Id:       entity.Id,
+		Username: entity.Username,
+		Email:    entity.Email,
+		Name:     entity.Username,
+		Country:  entity.Country.Name}
+
+	err = session.PutObject(r, EntityKey, sessionData)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return nil
+	}
+	return sessionData
+}
