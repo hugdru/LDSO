7db455ea8c25ab0205db04e3afb8b35892f70e3a
potter <up201306843@fe.up.pt>
2016-12-26 18:55:18 +0000

    home page template initiated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {AuditTemplateComponent} from "audit-template/audit-template.component";
+import {MainPageComponent} from "main-page/main-page.component";
 import {AuditComponent} from "audit/audit.component";
 import {ListPropertiesComponent} from "./property/list-properties.component";
 import {LoginComponent} from "./login/login.component";
@@ -9,8 +10,12 @@ import {RegisterComponent} from "./register/register.component";
 const routes: Routes = [
     {
         path: '',
-        redirectTo: '/auditTemplate', //change to homepage once implemented
+        redirectTo: '/main-page', //change to homepage once implemented
         pathMatch: 'full'
+    },
+    {
+        path: 'main-page',
+        component: MainPageComponent
     },
     {
         path: 'auditTemplate',
