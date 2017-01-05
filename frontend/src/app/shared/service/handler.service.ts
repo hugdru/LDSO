708fce4b8e50ce8737ee29a33b5afb31b5764764
francisquinha <francisquinha@gmail.com>
2017-01-05 22:18:58 +0000

    fixed handlers credentials bug

--- frontend/src/app/shared/service/handler.service.ts
+++ frontend/src/app/shared/service/handler.service.ts
@@ -39,7 +39,7 @@ export class HandlerService {
     }
 
     getAll<T>(url: string): Observable<T> {
-        return this.http.get(url)
+        return this.http.get(url, this.options)
                 .map((response: Response) => response.json())
                 .map((data: any) => {
                     let result: T = null;
@@ -51,7 +51,7 @@ export class HandlerService {
     }
 
     getResponse(url: string): Observable<Response> {
-        return this.http.get(url)
+        return this.http.get(url, this.options)
                 .map((response: Response) => response)
                 .catch(this.handleError);
     }
@@ -64,7 +64,7 @@ export class HandlerService {
 
     delete(url: string, id: number): Observable<Response> {
         let formated = url + "/" + id;
-        return this.http.delete(formated)
+        return this.http.delete(formated, this.options)
                 .map((response: Response) => response)
                 .catch(this.handleError);
     }
