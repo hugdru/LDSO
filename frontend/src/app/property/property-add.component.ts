c57fb9bfa90e6e7baa5595c76bc1d4730a9718ef
francisquinha <francisquinha@gmail.com>
2017-01-05 22:42:15 +0000

    fixed template close bug

--- frontend/src/app/property/property-add.component.ts
+++ frontend/src/app/property/property-add.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {PropertyService} from "./service/property.service";
 import {Property} from "./property";
 import {Address} from "./address";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'property-add',
@@ -12,7 +13,7 @@ import {Address} from "./address";
 export class PropertyAddComponent implements OnInit {
     selectedObject: Property;
 
-    constructor(private propertyService: PropertyService) {
+    constructor(private propertyService: PropertyService, private router: Router) {
 
     }
 
@@ -23,7 +24,10 @@ export class PropertyAddComponent implements OnInit {
 
     addProperty(): void {
         this.propertyService.setProperty(this.selectedObject).subscribe(
-                response => this.selectedObject.id = response.json().id
+                response => {
+                    this.selectedObject.id = response.json().id;
+                    this.router.navigate(['/listPropertiesView']);
+                }
         );
     }
 
